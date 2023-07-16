c5d0694a2a96d07afb1b1a7df240c77301aa5f3c
Hussain Ali <huali235@gmail.com>
2023-07-16 10:15:52 -0400

    results component

---
+++ tip-calculator-app/src/components/NumPeople.js
@@ -0,0 +1,8 @@
+export default function NumPeople() {
+  return (
+    <>
+      <h4>Number of People</h4>
+      <input className="num-people-input" type="text" placeholder="0" />
+    </>
+  );
+}
---
+++ tip-calculator-app/src/components/Results.js
@@ -0,0 +1,23 @@
+export default function Results() {
+  return (
+    <>
+      <div className="tip-result">
+        <p>
+          Tip Amount<br></br>
+          <span className="light-shade">/ person</span>
+        </p>
+        <p className="amount">$0.00</p>
+      </div>
+      <div></div>
+      <div className="total-result">
+        <p>
+          Total<br></br>
+          <span className="light-shade">/ person</span>
+        </p>
+        <p className="amount">$0.00</p>
+      </div>
+
+      <button>Reset</button>
+    </>
+  );
+}
